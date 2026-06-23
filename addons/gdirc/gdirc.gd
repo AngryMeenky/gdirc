5a220a054cc5d76f63585f200466710d6b33fa21
John Powell <Powell_John@bah.com>
2026-06-23 10:45:36 -0400

    Removed boilerplate comments

--- addons/gdirc/gdirc.gd
+++ addons/gdirc/gdirc.gd
@@ -3,10 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
 	add_custom_type("IRC", "Node", preload("res://addons/gdirc/Irc.gd"), preload("res://addons/gdirc/gdirc.png"))
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
 	remove_custom_type("IRC")
